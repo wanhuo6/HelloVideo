b63bb1b4619f96ed372916eda49e72c94c186085
huijie <857166619@qq.com>
2018-05-03 13:11:00 +0800

    first_commit

---
+++ app/src/main/java/com/ahuo/fire/hellovideo/Test.java
@@ -0,0 +1,16 @@
+package com.ahuo.fire.hellovideo;
+
+/**
+ * description :
+ * author : LiuHuiJie
+ * created on : 2018/5/3
+ */
+public class Test {
+
+    public void run(){
+        System.out.println("11");
+}
+    public void run2(){
+        System.out.println("11");
+    }
+}
---
+++ app/src/main/java/com/ahuo/fire/hellovideo/hello.java
@@ -0,0 +1,12 @@
+package com.ahuo.fire.hellovideo;
+
+/**
+ * description :
+ * author : LiuHuiJie
+ * created on : 2018/5/3
+ */
+public class hello {
+    public int  testRun(int i){
+        return i/2;
+    }
+}
